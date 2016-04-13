edb1278c912f80be9e650e44e060125954472c91
Pratik Vyas <m@pd.io>
2016-04-13 15:24:59 -0700

    Use a config struct

--- config.go
+++ config.go
@@ -1,30 +1,41 @@
 package main
 
 import (
-	"net/url"
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// func MakeBackendChannelMap(rules []ConfigRule) map[ConfigRuleId]chan string {
+// 	ret := make(map[ConfigRuleId]chan string)
+// 	for _, rule := range rules {
+// 		ret[rule.Id] = make(chan string)
+// 	}
+// 	ret
+// }
+
+var db *sql.DB
+
 type Config struct {
-	Port  int
-	Rules []ConfigRule
-}
-
-type ConfigRuleId int64
-
-type ConfigRule struct {
-	Id             ConfigRuleId
+	Id             int
 	Host           string
 	BackendServers []BackendServer
 }
 
 type BackendServer struct {
-	Url url.URL
+	Id       int
+	Host     string
+	ConfigId int
 }
 
-func MakeBackendChannelMap(rules []ConfigRule) map[ConfigRuleId]chan string {
-	ret := make(map[ConfigRuleId]chan string)
-	for _, rule := range rules {
-		ret[rule.Id] = make(chan string)
-	}
-	return ret
+func NewConfig(hostname string, backendServers []BackendServer) Config {
+	var config Config
+	config.BackendServers = backendServers
+	return config
+}
+
+func NewBackendServer(url string) BackendServer {
+	var backendServer BackendServer
+	backendServer.Host = url
+	return backendServer
 }
--- main.go
+++ main.go
@@ -3,30 +3,22 @@ package main
 import (
 	"net/http"
 	_ "net/http/pprof"
-	"net/url"
 )
 
-var defaultConfigRule ConfigRule
-var ruleBackendChannelMap map[ConfigRuleId]chan string
-
 var globalStatSink chan GlobalStatRecord
+var defaultConfig Config
 
 func main() {
 	globalStatSink = make(chan GlobalStatRecord)
-
-	proxy := NewSingleHostReverseProxy(&url.URL{
-		Scheme: "http",
-		Host:   "localhost:9091",
-		Path:   "/abcd/",
-	})
-
-	defaultConfigRule = ConfigRule{
-		Host:           "localhost:9091",
-		BackendServers: []string{"localhost:9090", "localhost:9089"},
-	}
-	ruleBackendChannelMap = MakeBackendChannelMap([]ConfigRule{defaultConfigRule})
+	proxy := &ReverseProxy{Director: director}
+	defaultConfig = NewConfig("localhost:9091", []BackendServer{NewBackendServer("localhost:9091"), NewBackendServer("localhost:9092")})
 
 	go statProcessor()
 	go reqsPrinter()
 	http.ListenAndServe(":9090", proxy)
+}
+
+func director(req *http.Request) {
+	req.URL.Scheme = "http"
+	req.URL.Host = defaultConfig.BackendServers[0].Host
 }
