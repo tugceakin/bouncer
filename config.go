613870a77541d6913afb1e078100b0e6d8d7943d
Pratik Vyas <m@pd.io>
2016-04-14 11:36:42 -0700

    Revert "Updated config.go and config_test.go without custom error handling"
    This reverts commit 6e4403ccea2355f792450456faf7d5299fdf86dd.

--- config.go
+++ config.go
@@ -1,11 +1,18 @@
-package config
+package main
 
 import (
 	"database/sql"
-	"errors"
+
 	_ "github.com/mattn/go-sqlite3"
-	"log"
 )
+
+// func MakeBackendChannelMap(rules []ConfigRule) map[ConfigRuleId]chan string {
+// 	ret := make(map[ConfigRuleId]chan string)
+// 	for _, rule := range rules {
+// 		ret[rule.Id] = make(chan string)
+// 	}
+// 	ret
+// }
 
 var db *sql.DB
 
@@ -41,203 +48,17 @@ func (config *Config) Destroy() {
 	config.done <- struct{}{}
 }
 
-func NewConfig(host string, backendServers []BackendServer) (Config, error) {
+func NewConfig(hostname string, backendServers []BackendServer) Config {
 	var config Config
-	if _, err := GetConfigId(host); err == nil {
-		log.Println(err)
-		return config, errors.New("Config already exists")
-	}
-	config.Host = host
 	config.BackendServers = backendServers
 	config.NextBackendServer = make(chan BackendServer)
 	config.done = make(chan struct{})
 	go config.NextBackendServerRoutine()
-	return config, nil
+	return config
 }
 
-func NewBackendServer(host string) BackendServer {
+func NewBackendServer(url string) BackendServer {
 	var backendServer BackendServer
-	backendServer.Host = host
+	backendServer.Host = url
 	return backendServer
-}
-
-func (config *Config) Save() error {
-	if config.Id == 0 {
-
-		tx, err := db.Begin()
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		stmt, err := tx.Prepare("insert into Config(host) values(?)")
-		if err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		defer stmt.Close()
-		if _, err = stmt.Exec(config.Host); err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-
-		id, err := GetConfigId(config.Host)
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-
-		tx, err = db.Begin()
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		stmt, err = tx.Prepare("delete from BackendServer where config_id = ?")
-		if err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		defer stmt.Close()
-		if _, err = stmt.Exec(id); err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-
-		for _, backendServer := range config.BackendServers {
-			backendServer.ConfigId = id
-			if err = backendServer.Save(); err != nil {
-				log.Println(err)
-				return err
-			}
-		}
-		return nil
-	} else {
-		tx, err := db.Begin()
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		stmt, err := tx.Prepare("delete from BackendServer where config_id = ?")
-		if err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		defer stmt.Close()
-		if _, err = stmt.Exec(config.Id); err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-
-		for _, backendServer := range config.BackendServers {
-			backendServer.ConfigId = config.Id
-			if err = backendServer.Save(); err != nil {
-				log.Println(err)
-				return err
-			}
-		}
-		return nil
-	}
-}
-
-func (backendServer *BackendServer) Save() error {
-	if backendServer.Id == 0 {
-
-		tx, err := db.Begin()
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		stmt, err := tx.Prepare("insert into BackendServer(host, config_id) values(?,?)")
-		if err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		defer stmt.Close()
-		_, err = stmt.Exec(backendServer.Host, backendServer.ConfigId)
-		if err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-		return nil
-
-	} else {
-
-		tx, err := db.Begin()
-		if err != nil {
-			log.Println(err)
-			return err
-		}
-		stmt, err := tx.Prepare("insert into BackendServer(id, host, config_id) values(?,?,?)")
-		if err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		defer stmt.Close()
-		if _, err = stmt.Exec(backendServer.Id, backendServer.Host, backendServer.ConfigId); err != nil {
-			log.Println(err)
-			tx.Rollback()
-			return err
-		}
-		tx.Commit()
-		return nil
-	}
-}
-
-func GetConfigId(host string) (int, error) {
-	stmt, err := db.Prepare("select id from Config where host = ?")
-	if err != nil {
-		log.Println(err)
-		return 0, err
-	}
-	defer stmt.Close()
-	row := stmt.QueryRow(host)
-
-	var id int
-	err = row.Scan(&id)
-	return id, err
-}
-
-func GetConfig(host string) (Config, error) {
-	var config Config
-	var err error
-	config.Id, err = GetConfigId(host)
-	if err != nil {
-		log.Println(err)
-		return config, err
-	}
-	config.Host = host
-
-	stmt, err := db.Prepare("select * from BackendServer where config_id = ?")
-	if err != nil {
-		log.Println(err)
-		return config, err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(config.Id)
-	if err != nil {
-		log.Println(err)
-		return config, err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var backendServer BackendServer
-		if err = rows.Scan(&backendServer.Id, &backendServer.Host, &backendServer.ConfigId); err != nil {
-			return config, err
-		}
-		config.BackendServers = append(config.BackendServers, backendServer)
-	}
-	return config, err
 }
