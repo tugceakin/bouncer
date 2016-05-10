87751d68e0215dbbbbabc3714ae1ad012ceff654
Ashwin Agrawal <ashwinagrawal1007@gmail.com>
2016-05-09 18:32:13 -0700

    Revert "Rule matching, Error handling pending"
    This reverts commit f68f40e8718060ad6b2ddc79f38bcf02904c058c.

--- config.go
+++ config.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-	"sort"
-	"sync"
-)
-
 type Config struct {
 	Id                            int
 	Host                          string
@@ -54,86 +47,4 @@ func NewBackendServer(url string) BackendServer {
 	var backendServer BackendServer
 	backendServer.Host = url
 	return backendServer
-}
-
-type HostConfigs struct {
-	configs []*Config
-	mutex   sync.Mutex
-}
-
-func (a HostConfigs) Len() int           { return len(a.configs) }
-func (a HostConfigs) Less(i, j int) bool { return len(a.configs[i].Path) > len(a.configs[j].Path) }
-func (a HostConfigs) Swap(i, j int)      { a.configs[i], a.configs[j] = a.configs[j], a.configs[i] }
-
-func (a *HostConfigs) AddConfig(config *Config) {
-	a.mutex.Lock()
-	a.configs = append(a.configs, config)
-	sort.Sort(a)
-	a.mutex.Unlock()
-}
-
-func (a *HostConfigs) RemoveConfig(config *Config) {
-	a.mutex.Lock()
-	for indx, val := range a.configs {
-		if val.Path == config.Path {
-			a.configs = append(a.configs[:indx], a.configs[indx+1:]...)
-			break
-		}
-	}
-	sort.Sort(a)
-	a.mutex.Unlock()
-}
-
-func (a *HostConfigs) UpdateConfig(config *Config) {
-	a.mutex.Lock()
-	for indx, val := range a.configs {
-		if val.Path == config.Path {
-			a.configs = append(a.configs[:indx], a.configs[indx+1:]...)
-			a.configs = append(a.configs, config)
-			break
-		}
-	}
-	sort.Sort(a)
-	a.mutex.Unlock()
-}
-
-func (a HostConfigs) Match(path string) *Config {
-	for _, config := range a.configs {
-		if match, _ := regexp.MatchString("^"+config.Path, path); match {
-			return config
-		}
-	}
-	return nil
-}
-
-type ConfigStore map[string]*HostConfigs
-
-func (a ConfigStore) AddConfig(config *Config) {
-	if _, ok := a[config.Host]; ok {
-		a[config.Host].AddConfig(config)
-	} else {
-		var hostConfigs HostConfigs
-		hostConfigs.AddConfig(config)
-		a[config.Host] = &hostConfigs
-	}
-}
-
-func (a ConfigStore) RemoveConfig(config *Config) {
-	if val, ok := a[config.Host]; ok {
-		val.RemoveConfig(config)
-	}
-}
-
-func (a ConfigStore) UpdateConfig(config *Config) {
-	if val, ok := a[config.Host]; ok {
-		val.UpdateConfig(config)
-	}
-}
-
-func (a ConfigStore) Match(target *url.URL) *Config {
-	var config *Config
-	if val, ok := a[target.Host]; ok {
-		return val.Match(target.Path)
-	}
-	return config
 }
